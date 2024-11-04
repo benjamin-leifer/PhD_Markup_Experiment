a95c8e6699a7db46ffebc9f791152e29edf2b3f6
benjamin-leifer <benjamin-leifer@users.noreply.github.com>
2024-11-03 20:45:27 -0500

    New Objects trial

--- Python Codes/Cell_Model/formulation.py
+++ Python Codes/Cell_Model/formulation.py
@@ -9,10 +9,10 @@ class Formulation(DatabaseObject):
     collection_name = "formulations"
 
     def __init__(self, name, date=None, materials=None, formulation_id=None):
-        super().__init__(db_name="inventory", collection_name="formulations")
+        super().__init__(self.db_name, self.collection_name)
         self.name = name
         self.date = date or datetime.now().strftime("%Y-%m-%d")
-        self.materials = materials or {}  # Dictionary with Material instances and quantities
+        self.materials = materials or {}
         self.formulation_id = formulation_id or self.generate_formulation_id()
 
     def generate_formulation_id(self):
@@ -21,27 +21,18 @@ class Formulation(DatabaseObject):
         return f"{self.name}_{timestamp}"
 
     def to_dict(self):
-        # Convert materials to a dictionary of material lot numbers and quantities
-        materials_dict = {material.lot_number: quantity for material, quantity in self.materials.items()}
         return {
             "name": self.name,
             "date": self.date,
-            "materials": materials_dict,
+            "materials": {material.lot_number: qty for material, qty in self.materials.items()},
             "formulation_id": self.formulation_id
         }
 
     def from_dict(self, document):
-        """Load formulation details from a MongoDB document."""
         self.name = document["name"]
         self.date = document["date"]
         self.formulation_id = document["formulation_id"]
-
-        # Load materials from MongoDB using the lot numbers provided in the document
-        self.materials = {}
-        for lot_number, qty in document["materials"].items():
-            material = Material.load_from_db({"lot_number": lot_number})  # Load Material by lot_number
-            if material:
-                self.materials[material] = qty
+        self.materials = {Material.load_from_db({"lot_number": lot}): qty for lot, qty in document["materials"].items()}
 
     def add_gui_components(self, root):
         from tkinter import Label
